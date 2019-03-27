b49306dde1ee0e7f5eae9cc2b65c7bae40ff37ea
Jonah Moses <jonahkmoses@gmail.com>
2019-03-26 17:31:45 -0700

    add minitest-focus

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,3 @@
 require_relative "../lib/timekit"
 require "minitest/autorun"
+require "minitest/focus"
--- test/timekit_test.rb
+++ test/timekit_test.rb
@@ -1,11 +1,9 @@
 require_relative "./test_helper"
 
 class TimekitTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Timekit::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
+  describe "foo" do
+    it "to bar'd" do
+      assert false
+    end
   end
 end
