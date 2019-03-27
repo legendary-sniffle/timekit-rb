c92aac4fc923ec2038d96087368d4c44bd8b218c
Jonah Moses <jonahkmoses@gmail.com>
2019-03-26 17:41:27 -0700

    add version test

--- test/timekit_test.rb
+++ test/timekit_test.rb
@@ -2,6 +2,10 @@ require_relative "./test_helper"
 
 class TimekitTest < Minitest::Test
   describe "foo" do
+    it "has a version number" do
+      refute_nil Timekit::VERSION
+    end
+
     it "does something falsy" do
       refute false
     end
