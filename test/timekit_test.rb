345d5fbe486a7af529c2ff57486e79523a65c03c
Jonah Moses <jonahkmoses@gmail.com>
2019-03-26 17:33:39 -0700

    add minitest-focus

--- test/timekit_test.rb
+++ test/timekit_test.rb
@@ -2,8 +2,12 @@ require_relative "./test_helper"
 
 class TimekitTest < Minitest::Test
   describe "foo" do
-    it "to bar'd" do
-      assert false
+    it "does something falsy" do
+      refute false
+    end
+
+    it "does something truthy" do
+      assert true
     end
   end
 end
