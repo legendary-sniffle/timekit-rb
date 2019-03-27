edc4d0115f4992d7f17ede94539de30f65d3c2be
Jonah Moses <jonahkmoses@gmail.com>
2019-03-26 17:23:06 -0700

    adjust configurations to simply basic_auth_key

--- lib/timekit/configuration.rb
+++ lib/timekit/configuration.rb
@@ -1,26 +1,14 @@
 module Timekit
   class Configuration
-    attr_accessor :access_key, :secret_key, :personal_key
+    attr_accessor :basic_auth_key
 
     def initialize
-      @access_key = nil
-      @secret_key = nil
-      @personal_key = nil
+      @basic_auth_key = nil
     end
 
-    def access_key
-      raise Errors::Configuration, "Timekit access key missing!" unless @access_key
-      @access_key
-    end
-
-    def secret_key
-      raise Errors::Configuration, "Timekit secret key missing!" unless @secret_key
-      @secret_key
-    end
-
-    def personal_key
-      raise Errors::Configuration, "Timekit personal key missing!" unless @personal_key
-      @personal_key
+    def basic_auth_key
+      raise Errors::Configuration, "Timekit requires basic authentication key!" unless @basic_auth_key
+      @basic_auth_key
     end
   end
 end
